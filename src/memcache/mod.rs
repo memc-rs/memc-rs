ec83f5efc234852608fc624c3ee58593bb9eccfc
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-04-06 22:20:00 +0200

    Added timer module to provide time in secs

--- src/memcache/mod.rs
+++ src/memcache/mod.rs
@@ -1,3 +1,4 @@
 pub mod handler;
 pub mod server;
 pub mod storage;
+pub mod timer;
---
+++ src/memcache/timer.rs
@@ -0,0 +1,19 @@
+
+pub trait Timer {
+    fn secs(&self) -> u64;
+}
+
+pub struct SystemTimer;
+
+impl SystemTimer {
+    pub fn new() -> Self {
+        SystemTimer{            
+        }
+    }
+}
+
+impl Timer for SystemTimer {
+    fn secs(&self) -> u64 {
+        0
+    }
+}
