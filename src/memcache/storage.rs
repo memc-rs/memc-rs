36067012c1a9edfcefadc5bbe40c7f9119ce0588
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-05-19 21:51:28 +0200

    Fixed issue with deadlock when checking for record expiration

--- src/memcache/storage.rs
+++ src/memcache/storage.rs
@@ -2,7 +2,8 @@ use super::error::{StorageError, StorageResult};
 use super::timer;
 use dashmap::DashMap;
 use std::str;
-use std::sync::{Arc, RwLock};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
 
 #[derive(Clone, Debug)]
 pub struct Header {
@@ -53,7 +54,7 @@ pub struct Storage {
     memory: dashmap::DashMap<Vec<u8>, Record>,
     timer: Arc<dyn timer::Timer + Send + Sync>,
 }
-
+#[derive(Debug)]
 pub struct SetStatus {
     pub cas: u64,
 }
@@ -72,19 +73,25 @@ impl Storage {
     }
 
     fn get_by_key(&self, key: &Vec<u8>) -> StorageResult<Record> {
-        match self.memory.get_mut(key) {
-            Some(mut record) => {
-                if self.check_if_expired(key, &mut record) {
-                    Err(StorageError::NotFound)
-                } else {
-                    Ok(record.clone())
-                }
+        let result = match self.memory.get(key) {
+            Some(record) => {
+                Ok(record.clone())                
             }
             None => Err(StorageError::NotFound),
-        }
+        };
+
+        match result {
+            Ok(record) => {
+                if self.check_if_expired(key, &record) {
+                    return Err(StorageError::NotFound);
+                }
+                Ok(record)
+            },
+            Err(err) => Err(err)
+        }        
     }
 
-    fn check_if_expired(&self, key: &Vec<u8>, record: &mut Record) -> bool {
+    fn check_if_expired(&self, key: &Vec<u8>, record: &Record) -> bool {
         let current_time = self.timer.secs();
 
         if record.header.expiration == 0 {
@@ -94,12 +101,14 @@ impl Storage {
         if record.header.timestamp + (record.header.expiration as u64) > current_time {
             return false;
         }
-        self.memory.remove(key);
-        true
+        match self.memory.remove(key) {
+            Some(_) => true,
+            None => true
+        }        
     }
 
     fn touch_record(&self, record: &mut Record) {
-        let timer = self.timer.secs();        
+        let timer = self.timer.secs();
     }
 
     pub fn set(&self, key: Vec<u8>, mut record: Record) -> StorageResult<SetStatus> {
@@ -156,66 +165,60 @@ mod tests {
     use std::fs::File;
 
     struct MockSystemTimer {
-        current_time: u64
+        current_time: AtomicUsize,
     }
 
-    trait SetableTimer : timer::Timer {
-        fn set(&mut self, time: u64);    
+    trait SetableTimer: timer::Timer {
+        fn set(&self, time: u64);
     }
 
     impl MockSystemTimer {
         pub fn new() -> Self {
             MockSystemTimer {
-                current_time: 0
+                current_time: AtomicUsize::new(0),
             }
         }
     }
 
     impl timer::Timer for MockSystemTimer {
         fn secs(&self) -> u64 {
-            self.current_time
+            self.current_time.load(Ordering::Relaxed) as u64
         }
     }
 
     impl SetableTimer for MockSystemTimer {
-        fn set(&mut self, time: u64) {
-            self.current_time = time
+        fn set(&self, time: u64) {
+            self.current_time.store(time as usize, Ordering::Relaxed)
         }
     }
-    
+
     struct MockServer {
-        timer: Arc<dyn SetableTimer + Send + Sync>,
-        storage: Storage
+        pub timer: Arc<MockSystemTimer>,
+        pub storage: Storage,
     }
 
     impl MockServer {
         pub fn new() -> Self {
-            let timer: Arc<dyn SetableTimer + Send + Sync> = Arc::new(MockSystemTimer::new());
+            let timer = Arc::new(MockSystemTimer::new());
             MockServer {
                 timer: timer.clone(),
-                storage: Storage::new(timer as Arc<dyn timer::Timer+Send+Sync>)
+                storage: Storage::new(timer),
             }
         }
     }
-    fn create_timer() -> Arc<dyn timer::Timer + Send + Sync> {
-        Arc::new(MockSystemTimer::new())
-    }
 
-    fn create_storage() -> Storage {
-        let timer: Arc<dyn timer::Timer + Send + Sync> = create_timer();
-        Storage::new(timer)
+    fn create_server() -> MockServer {
+        MockServer::new()
     }
-
-    
 
     #[test]
     fn if_not_defined_cas_should_be_1() {
-        let storage = create_storage();
+        let server = create_server();
         let key = String::from("key").into_bytes();
         let record = Record::new(String::from("Test data").into_bytes(), 0, 0, 0);
-        let result = storage.set(key.clone(), record.clone());
+        let result = server.storage.set(key.clone(), record.clone());
         assert!(result.is_ok());
-        let found = storage.get(&key);
+        let found = server.storage.get(&key);
         assert!(found.is_ok());
         match found {
             Ok(r) => {
@@ -228,7 +231,7 @@ mod tests {
 
     #[test]
     fn if_cas_defined_it_should_be_returned() {
-        let storage = create_storage();
+        let storage = create_server().storage;
         let cas: u64 = 0xDEAD_BEEF;
         let key = String::from("key").into_bytes();
         let record = Record::new(String::from("Test data").into_bytes(), cas, 0, 0);
@@ -248,7 +251,7 @@ mod tests {
 
     #[test]
     fn insert_should_fail_on_cas_mismatch() {
-        let storage = create_storage();
+        let storage = create_server().storage;
         let cas: u64 = 0xDEAD_BEEF;
         let key = String::from("key").into_bytes();
         let mut record = Record::new(String::from("Test data").into_bytes(), cas, 0, 0);
@@ -259,6 +262,29 @@ mod tests {
         match result {
             Ok(_) => unreachable!(),
             Err(err) => assert_eq!(err, StorageError::KeyExists),
+        }
+    }
+
+    #[test]
+    fn record_should_expire_in_given_time() {
+        let server = create_server();
+        let cas: u64 = 0xDEAD_BEEF;
+        let key = String::from("key").into_bytes();
+        let record = Record::new(String::from("Test data").into_bytes(), cas, 0, 123);
+        let result = server.storage.set(key.clone(), record.clone());
+        assert!(result.is_ok());
+        println!("{:?}", result);
+        let found = server.storage.get(&key);
+        assert!(found.is_ok());
+
+        server.timer.set(128);
+        let found = server.storage.get(&key);
+        assert!(found.is_err());
+        match found {
+            Ok(_r) => {
+                unreachable!()
+            }
+            Err(err) => assert_eq!(err, StorageError::NotFound),
         }
     }
 }
