2974177c896c0502c6f2fd283f06c5f30901cad5
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-05-20 21:05:11 +0200

    Added timeout for read/write from network

--- src/memcache/server.rs
+++ src/memcache/server.rs
@@ -1,10 +1,12 @@
 use futures_util::sink::SinkExt;
 use std::error::Error;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
-use std::sync::{Arc, RwLock};
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::sync::Arc;
+use std::time::Duration;
 use tokio::io;
 use tokio::net::{TcpListener, TcpStream, ToSocketAddrs as TokioToSocketAddrs};
 use tokio::stream::{Stream, StreamExt as TokioStreamExt};
+use tokio::time::{interval_at, timeout, Instant};
 use tokio_util::codec::{FramedRead, FramedWrite};
 
 use super::handler;
@@ -13,20 +15,47 @@ use super::timer;
 use crate::protocol::binary_codec;
 
 pub struct TcpServer {
-    timer: Arc<dyn timer::Timer + Send + Sync>,
+    timer: Arc<timer::SystemTimer>,
     storage: Arc<storage::Storage>,
+    timeout_secs: u64,
 }
 
 impl Default for TcpServer {
     fn default() -> Self {
-        let timer: Arc<dyn timer::Timer + Send + Sync> = Arc::new(timer::SystemTimer::new());
+        let timer = Arc::new(timer::SystemTimer::new());
         TcpServer {
+            timeout_secs: 60,
             timer: timer.clone(),
-            storage: Arc::new(storage::Storage::new(timer.clone())),
+            storage: Arc::new(storage::Storage::new(timer)),
         }
     }
 }
 
+struct Client {
+    store: Arc<storage::Storage>,
+    socket: TcpStream,
+    addr: SocketAddr,
+    rx_timeout_secs: u64,
+    wx_timeout_secs: u64,
+}
+
+impl Client {
+    pub fn new(
+        store: Arc<storage::Storage>,
+        socket: TcpStream,
+        addr: SocketAddr,
+        rx_timeout_secs: u64,
+        wx_timeout_secs: u64,
+    ) -> Self {
+        Client {
+            store,
+            socket,
+            addr,
+            rx_timeout_secs,
+            wx_timeout_secs,
+        }
+    }
+}
 impl TcpServer {
     pub fn new() -> TcpServer {
         Default::default()
@@ -34,49 +63,71 @@ impl TcpServer {
 
     pub async fn run<A: ToSocketAddrs + TokioToSocketAddrs>(&mut self, addr: A) -> io::Result<()> {
         let mut listener = TcpListener::bind(addr).await?;
+        let start = Instant::now();
+        let mut interval = interval_at(start, Duration::from_millis(10));
         loop {
             match listener.accept().await {
                 Ok((mut socket, peer_addr)) => {
-                    let db = self.storage.clone();
+                    let client = Client::new(
+                        self.storage.clone(),
+                        socket,
+                        peer_addr,
+                        self.timeout_secs,
+                        self.timeout_secs,
+                    );
                     info!("Incoming connection: {}", peer_addr);
                     // Like with other small servers, we'll `spawn` this client to ensure it
                     // runs concurrently with all other clients. The `move` keyword is used
                     // here to move ownership of our db handle into the async closure.
-                    tokio::spawn(async move {
-                        let mut handler = handler::BinaryHandler::new(db);
-                        // Since our protocol is line-based we use `tokio_codecs`'s `LineCodec`
-                        // to convert our stream of bytes, `socket`, into a `Stream` of lines
-                        // as well as convert our line based responses into a stream of bytes.
-                        let (rx, tx) = socket.split();
-
-                        let mut reader =
-                            FramedRead::new(rx, binary_codec::MemcacheBinaryCodec::new());
-                        let mut writer =
-                            FramedWrite::new(tx, binary_codec::MemcacheBinaryCodec::new());
-
-                        // Here for every packet we get back from the `Framed` decoder,
-                        // we parse the request, and if it's valid we generate a response
-                        // based on the values in the database.
-                        while let Some(result) = reader.next().await {
-                            match result {
-                                Ok(request) => {
-                                    let response = handler.handle_request(request);
-                                    if let Some(response) = response {
-                                        if let Err(e) = writer.send(response).await {
-                                            error!("error on sending response; error = {:?}", e);
-                                        }
-                                    }
-                                }
-                                Err(e) => {
-                                    error!("error on decoding from socket; error = {:?}", e);
-                                }
-                            }
-                        }
-
-                        // The connection will be closed at this point as `lines.next()` has returned `None`.
-                    });
+                    tokio::spawn(async move { TcpServer::handle_client(client).await });
                 }
                 Err(e) => error!("error accepting socket; error = {:?}", e),
+            }
+        }
+    }
+
+    async fn handle_client(mut client: Client) {
+        let mut handler = handler::BinaryHandler::new(client.store);
+        //
+        let (rx, tx) = client.socket.split();
+
+        let mut reader = FramedRead::new(rx, binary_codec::MemcacheBinaryCodec::new());
+        let mut writer = FramedWrite::new(tx, binary_codec::MemcacheBinaryCodec::new());
+
+        // Here for every packet we get back from the `Framed` decoder,
+        // we parse the request, and if it's valid we generate a response
+        // based on the values in the database.
+        loop {
+            match timeout(Duration::from_secs(client.rx_timeout_secs), reader.next()).await {
+                Ok(req_or_none) => {
+                    match req_or_none {
+                        Some(req_or_error) => match req_or_error {
+                            Ok(request) => {
+                                let response = handler.handle_request(request);
+                                if let Some(response) = response {
+                                    if let Err(e) = timeout(Duration::from_secs(client.rx_timeout_secs), writer.send(response)).await {
+                                        error!("error on sending response; error = {:?}", e);
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                error!("error on decoding from socket; error = {:?}", e);
+                            }
+                        },
+                        None => {
+                            // The connection will be closed at this point as `lines.next()` has returned `None`.
+                            info!("Conneciton closed: {}", client.addr);
+                            return;
+                        }
+                    }
+                }
+                Err(err) => {
+                    info!(
+                        "Timeout {}s elapsed, disconecting client: {}, error: {}",
+                        client.rx_timeout_secs, client.addr, err
+                    );
+                    return;
+                }
             }
         }
     }
