cc5e1a6a2a9699b4d880fb29271c391ce1702c0d
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-05-20 21:06:33 +0200

    Implemented Setable timer to be able to mark time progress

--- src/memcache/timer.rs
+++ src/memcache/timer.rs
@@ -1,17 +1,33 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 pub trait Timer {
     fn secs(&self) -> u64;
 }
 
-pub struct SystemTimer;
+pub trait SetableTimer {
+    fn add_second(&self);
+}
+
+pub struct SystemTimer {
+    seconds: AtomicUsize,
+}
 
 impl SystemTimer {
     pub fn new() -> Self {
-        SystemTimer {}
+        SystemTimer {
+            seconds: AtomicUsize::new(0),
+        }
     }
 }
 
 impl Timer for SystemTimer {
     fn secs(&self) -> u64 {
-        0
+        self.seconds.load(Ordering::Relaxed) as u64
+    }
+}
+
+impl SetableTimer for SystemTimer {
+    fn add_second(&self) {
+        self.seconds.fetch_add(1, Ordering::Relaxed);
     }
 }
