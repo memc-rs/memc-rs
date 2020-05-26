7c7d92889e1ed6a99d2b231c3446ac8db908f366
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-05-26 22:41:15 +0200

    Enabled logger

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,18 @@
+use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
+use std::fs::File;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::io;
+
 extern crate memix;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    CombinedLogger::init(vec![WriteLogger::new(
+        LevelFilter::Debug,
+        Config::default(),
+        File::create("memix.log").unwrap(),
+    )])
+    .unwrap();
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 11211);
     let mut tcp_server = memix::memcache::server::TcpServer::new();
     tcp_server.run(addr).await
