756459909d7a52b7aedbef6b4e683460256ae1be
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-08-10 16:49:20 +0200

    Migrated to tracing from simplelog

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,4 @@
-use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
+
 use std::fs::File;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::io;
@@ -7,13 +7,7 @@ extern crate memix;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    CombinedLogger::init(vec![WriteLogger::new(
-        LevelFilter::Off,
-        Config::default(),
-        File::create("memix.log").unwrap(),
-    )])
-    .unwrap();
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 11211);
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 11211);
     let mut tcp_server = memix::memcache::server::TcpServer::new();
     tcp_server.run(addr).await
 }
