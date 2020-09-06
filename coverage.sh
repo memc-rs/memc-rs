ebbbe2aeacaf30c0cc449edaae94ed2c6fc2665a
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-09-06 18:17:47 +0200

    Added script for generating unit tests coverage

---
+++ coverage.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+cargo clean
+cargo +nightly test
+grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
