5f5566f8f88fa7020efb6f7fcb6eb92f1522387f
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-11-15 20:47:13 +0100

    Added rust client memcache for integration tests

---
+++ memclt/src/main.rs
@@ -0,0 +1,28 @@
+use memcache;
+use std::collections::HashMap;
+use std::str;
+
+fn main() {
+    let mut client = memcache::Client::connect(
+        "memcache://127.0.0.1:11211?timeout=10&tcp_nodelay=true&protocol=binary",
+    )
+    .unwrap();
+    let result: HashMap<String, (Vec<u8>, u32, Option<u64>)> = client.gets(&["foo"]).unwrap();
+    let (key, val, cas) = result.get("foo").unwrap();
+
+    println!(
+        "Foo: {:?} {} {}",
+        str::from_utf8(key).unwrap(),
+        val,
+        cas.unwrap()
+    );
+    
+    client.set("foo", "3000", 50).unwrap();
+        
+    println!(
+        "Foo: {:?} {} {}",
+        str::from_utf8(key).unwrap(),
+        val,
+        cas.unwrap()
+    );
+}
