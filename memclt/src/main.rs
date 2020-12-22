b8b9d638cbf9d153e3280e8d2736b0e5abac1777
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2020-12-22 16:10:18 +0100

    Added get on non existing key

--- memclt/src/main.rs
+++ memclt/src/main.rs
@@ -1,4 +1,4 @@
-use memcache::Client;
+use memcache::{Client, MemcacheError};
 use std::collections::HashMap;
 use std::str;
 
@@ -39,17 +39,22 @@ fn main() {
 
     client.decrement("foo", 50).unwrap();
 
-    let result: HashMap<String, (Vec<u8>, u32, Option<u64>)> = client.gets(&["foo"]).unwrap();
-    let (key, val, cas) = result.get("foo").unwrap();
-
-    println!(
-        "Foo: {:?} {} {}",
-        str::from_utf8(key).unwrap(),
-        val,
-        cas.unwrap()
-    );
 
     client.delete("foo").unwrap();
+
+    let result: Result<Option<String>, MemcacheError> = client.get("foo");
+    match result {
+        Ok(val) => {
+            match val {
+                Some(value) => println!("Server returned: {}", value),
+                None => println!("Server none"),
+            }
+        },
+        Err(err) => {
+            println!("Error returned: {:?}", err);
+        }
+    }
+
 
     client.flush_with_delay(100).unwrap();
 
