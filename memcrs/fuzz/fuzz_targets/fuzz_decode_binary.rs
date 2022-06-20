5e4478a7aa40ec295fdd06d30b196bbffa3cee4b
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2022-06-20 21:37:00 +0200

    NIT: Fixed suzzer issues

--- memcrs/fuzz/fuzz_targets/fuzz_decode_binary.rs
+++ memcrs/fuzz/fuzz_targets/fuzz_decode_binary.rs
@@ -6,7 +6,7 @@ use tokio_util::codec::{Decoder};
 
 fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
-    let mut codec = memcrs::protocol::binary_codec::MemcacheBinaryCodec::new();
+    let mut codec = memcrs::protocol::binary_codec::MemcacheBinaryCodec::new(1024);
     let mut src = BytesMut::with_capacity(data.len());
     src.put(data);
     let _ = codec.decode(&mut src);
