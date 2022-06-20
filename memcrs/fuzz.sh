e2a4447cce7d0ecaf842c800e45f312ad767c4bd
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2022-06-20 21:40:22 +0200

    NIT: Fixed fuzzer issues

--- memcrs/fuzz.sh
+++ memcrs/fuzz.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 export RUST_BACKTRACE=full
-cargo +nightly fuzz run -j 8 fuzz_binary_encoder --  -rss_limit_mb=4192 -timeout=60
+export RUSTFLAGS="-Znew-llvm-pass-manager=no"
+cargo +nightly fuzz run -j 8 fuzz_binary_decoder --  -rss_limit_mb=4192 -timeout=60
 
