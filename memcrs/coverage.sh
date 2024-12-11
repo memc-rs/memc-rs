635df3be1f70f0a828703377ed16f4eac7c2edcf
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2024-12-11 21:33:22 +0100

    NIT: Coverage script adjusted

--- memcrs/coverage.sh
+++ memcrs/coverage.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTFLAGS="-Cinstrument-coverage -Cprofile-generate -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
 cargo clean
 cargo +nightly test
-grcov ../target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ../target/debug/coverage/
+#grcov ../target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ../target/debug/coverage/
