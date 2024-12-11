4cec9c49bf97e6fc299b37172386e568c00fccb3
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2024-12-11 21:20:29 +0100

    NIT: Deps upgrade

--- memcrs/fuzz.sh
+++ memcrs/fuzz.sh
@@ -2,5 +2,6 @@
 
 export RUST_BACKTRACE=full
 #export RUSTFLAGS="-Znew-llvm-pass-manager=no"
-cargo +nightly fuzz run -j 8 fuzz_binary_decoder --  -rss_limit_mb=4192 -timeout=60
+cargo clean
+cargo fuzz run -j 8 fuzz_binary_decoder --  -rss_limit_mb=4192 -timeout=60
 
