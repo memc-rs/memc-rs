df0e642dd5f6195eb4569cedbedadf2802cecac6
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2025-01-13 21:23:32 +0100

    Added test for maximum value size

--- memcrs/tests/set_get.rs
+++ memcrs/tests/set_get.rs
@@ -1,4 +1,5 @@
 procspawn::enable_test_support!();
+use rand::Rng;
 mod common;
 
 #[test]
@@ -41,4 +42,31 @@ fn set_gets_check() {
     assert_eq!(result["foo1"], "bar1");
     assert_eq!(result["foo2"], "bar2");
     assert_eq!(result["foo3"], "bar3");
+}
+
+#[test]
+fn max_item_check() {
+    let params_builder: common::MemcrsdServerParamsBuilder = common::MemcrsdServerParamsBuilder::new();
+    let server_handle = common::spawn_server(params_builder);
+    let client =
+        memcache::connect(server_handle.get_connection_string())
+            .unwrap();
+    let mut rng = rand::thread_rng();
+    
+    // flush the database
+    client.flush().unwrap();
+
+    let max_item_size = 1048565; // 3 characters reserved for key "foo" and binary protocol data
+    let mut value =  String::with_capacity(max_item_size);
+    for _ in 0..max_item_size {
+        let random_char = rng.gen_range(b'a'..=b'z') as char;
+        value.push(random_char);
+    }
+    // set a string value
+    client.set("foo", &value, 0).unwrap();
+    
+    // retrieve from memcached:
+    let server_value: Option<String> = client.get("foo").unwrap();
+    assert_eq!(server_value, Some(value.clone()));
+    assert_eq!(server_value.unwrap(), value);
 }
